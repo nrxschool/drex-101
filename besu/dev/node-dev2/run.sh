aeb023a000c9a3cfa4ed4a55104a0f7801806184
Lucas Oliveira <olivmath@protonmail.com>
2024-04-11 15:45:35 -0300

    update: nodes

---
+++ besu/dev/node-dev2/run.sh
@@ -0,0 +1,3 @@
+besu \
+    --config-file=besu.toml \
+    --bootnodes=enode://dca9f58eed6dc98b4d0209b46cf70c3e086cbd0675a314d495d4a2c5edf6dc2b25822894c7a463c5b14ac6f67f24a29bd0326f018e35f8fe6d21249ee6df8dc8@127.0.0.1:30303
---
+++ besu/node4/run.sh
@@ -0,0 +1,10 @@
+besu \
+--data-path=data \
+--genesis-file=./genesis.json \
+--p2p-port=30306 \
+--rpc-http-enabled \
+--rpc-http-api=ETH,NET,QBFT \
+--host-allowlist="*" \
+--rpc-http-cors-origins="all" \
+--rpc-http-port=8548 \
+--bootnodes=enode://dca9f58eed6dc98b4d0209b46cf70c3e086cbd0675a314d495d4a2c5edf6dc2b25822894c7a463c5b14ac6f67f24a29bd0326f018e35f8fe6d21249ee6df8dc8@127.0.0.1:30303
