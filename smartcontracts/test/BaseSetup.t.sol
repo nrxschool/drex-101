1f401947d4b707bee05618679e8095196607d167
Lucas Oliveira <olivmath@protonmail.com>
2024-07-28 15:37:20 -0300

    update: base setup to generate all needed for test

--- smartcontracts/test/BaseSetup.t.sol
+++ smartcontracts/test/BaseSetup.t.sol
@@ -1,32 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import {Counter} from "../src/Counter.sol";
-import {Utils} from "./Utils.t.sol";
-
 import {RealDigital} from "../src/RealDigital.sol";
+import {RealTokenizado} from "../src/RealTokenizado.sol";
+import {Swap} from "../src/Swap.sol";
+import {STR} from "../src/STR.sol";
 
+import {Utils} from "./Utils.t.sol";
 import {console2} from "forge-std/Script.sol";
 
 contract BaseSetup is Utils {
-    RealDigital realDigital;
+    uint256 constant CENTAVOS = 100;
+
+    RealTokenizado rtNubank;
+    RealTokenizado rtItau;
+    RealDigital rd;
+    Swap swap;
+    STR str;
+
+    address itauBank = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+    address nubankBank = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+
+    address[] participants = [itauBank, nubankBank];
 
     address[] users;
-    address deployer;
-    address charles;
-    address alice;
-    address bob;
+    address bacen;
+    address charlesItau;
+    address aliceItau;
+    address bobNubank;
 
     function createMockUsers(address[] memory myUsers) private {
-        deployer = myUsers[2];
-        charles = myUsers[3];
-        alice = myUsers[0];
-        bob = myUsers[1];
+        bacen = myUsers[2];
+        charlesItau = myUsers[3];
+        aliceItau = myUsers[0];
+        bobNubank = myUsers[1];
 
-        vm.label(deployer, "deployer (dev)");
-        vm.label(charles, "charler");
-        vm.label(alice, "alice");
-        vm.label(bob, "bob");
+        vm.label(bacen, "Banco Central (deployer)");
+        vm.label(charlesItau, "Charles cliente Itau");
+        vm.label(aliceItau, "Alice cliente Itau");
+        vm.label(bobNubank, "Bob cliente Nubank");
+
+        vm.label(address(rtItau), "Real Tokenizado Itau");
+        vm.label(address(rtNubank), "Real Tokenizado Nubank");
+
+        vm.label(itauBank, "Banco Itau");
+        vm.label(nubankBank, "Banco Nubank");
     }
 
     function setUp() public virtual {
@@ -34,5 +52,30 @@ contract BaseSetup is Utils {
         users = utils.createUsers(6);
 
         createMockUsers(users);
+
+        vm.startPrank(bacen);
+        // Deploy Real Digital
+        rd = new RealDigital();
+
+        // Deploy STR and Swap contracts
+        str = new STR(address(rd), participants);
+        swap = new Swap(address(rd), participants);
+
+        // Set STR and Swap on Real Digital to mint and transfer balances
+        rd.setSTR(address(str));
+        rd.setSwap(address(swap));
+
+        // Deploy Real Tokenizado to Itau and Nubank
+        rtItau = new RealTokenizado(itauBank, "Itau", "ITAU");
+        rtNubank = new RealTokenizado(nubankBank, "Nubank", "NU");
+        vm.stopPrank();
+
+        // Itau set Swap on Real Tokenizado to transfer balances
+        vm.prank(itauBank);
+        rtItau.setSwap(address(swap));
+
+        // Nubank set Swap on Real Tokenizado to transfer balances
+        vm.prank(nubankBank);
+        rtNubank.setSwap(address(swap));
     }
 }
